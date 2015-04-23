725c0dcc07e932fb3003ec5afbc67e07012e5cfe
Manjae Lee <manjae.lee@lge.com>
2015-04-23 12:27:10 +0900

    finished the new footer [issue 53]

--- pthread_test1.c
+++ pthread_test1.c
@@ -14,7 +14,7 @@ int thread_args[4] = {0, 1, 2, 3};  /* 쓰레드가 사용할 인자 */
 void* Thread( void *arg )
 {
     int i;
-    for ( i=0; i<10; i++ )
+    for ( i=0; i<40; i++ )
         printf( "thread %d: %dth iteration\n", *(int*)arg, i );
     pthread_exit(0);  /* 쓰레드 종료 함수 */
 }
