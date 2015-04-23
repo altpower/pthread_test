05a80a3df265050a5d9e8c966b47bec6ebbe0a58
Manjae Lee <manjae.lee@lge.com>
2015-04-23 12:23:01 +0900

    modified count for loop

--- pthread_test1.c
+++ pthread_test1.c
@@ -8,13 +8,13 @@
 #include <stdio.h>
 #include <pthread.h>
 
-int thread_args[3] = { 0, 1, 2, 3};  /* 쓰레드가 사용할 인자 */
+int thread_args[4] = {0, 1, 2, 3};  /* 쓰레드가 사용할 인자 */
 //-------------------------------------------------------------
 /* 쓰레드로 수행할 함수 */
 void* Thread( void *arg )
 {
     int i;
-    for ( i=0; i<30; i++ )
+    for ( i=0; i<10; i++ )
         printf( "thread %d: %dth iteration\n", *(int*)arg, i );
     pthread_exit(0);  /* 쓰레드 종료 함수 */
 }
